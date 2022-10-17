a6a1645bf6c8109bc800056c8b66333d5573b7fd
damm <damm@192.168.208.170>
2022-10-17 11:10:57 +0200

    Colores

---
+++ src/org/daw/aad/capitulo01/aadcapitulo1b/ejercicio1/Ejercicio1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package org.daw.aad.capitulo01.aadcapitulo1b.ejercicio1;
+
+/**
+ *
+ * @author damm
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
